efac22589203c775a6f016d4696b069988125e3d
Y2498378289 <Y2498378289@noreply.gitcode.com>
2026-01-21 15:33:36 +0800

    手动实现动态数组

--- MyArrayList.cpp
+++ MyArrayList.cpp
@@ -4,20 +4,20 @@ using namespace std;
 
 template<typename E> // E是一个万能的类型占位符
 
-class MyArrayList() {
+class MyArrayList {
 private:
     E* data;
     int size;  // 当前元素数量
     int cap;
     static const int INIT_CAP=1; // 初始容量
 
-pubilc:
+public:
     MyArrayList() {
         this->data = new E[INIT_CAP];
         this->size = 0;
         this->cap = INIT_CAP;
     }
-    MyArrayList(int intiCap) {
+    MyArrayList(int initCap) {
         this->data = new E[initCap];
         this->size = 0;
         this->cap = initCap;
@@ -32,8 +32,8 @@ pubilc:
     }
 
     void add(int index, E e) {
-        if(index > cap) {
-            return 0;
+        if(index > cap) {  
+            return;  
         }
         if(size==cap) {
             resize(2 * cap);
@@ -44,7 +44,9 @@ pubilc:
         data[index] = e;
         size++; 
     }
-
+    void addFirst(E e) {
+        add(0, e);
+    }
     //删
     E removeLast() {
         if(size==0) {
@@ -67,8 +69,8 @@ pubilc:
             resize(cap / 2);
         }
         E deleteVal = data[index];
-        for(int i = size-1; i>=index; i--) {
-            data[i-1] = data[i];
+        for(int i = index; i < size - 1; i++) {  // 修复: 从index向后遍历
+            data[i] = data[i+1];  // 修复: 将后面的元素向前移动
         }
         data[size-1] = E();
         size--;
@@ -79,10 +81,60 @@ pubilc:
     }
 
     // 查
+    E get(int index) {
+        if(index >= size) {
+            return 0;
+        }
+        return data[index];
+    }
+
+    // 改
+    E set(int index, E element) {
+        if(index >= size) {
+            return 0;
+        }
+        E oldVal = data[index];
+        data[index] = element;
+        return oldVal;
+    }
+
+    // 工具方法
+    int getSize() {
+        return size;
+    }
     
+    void resize(int newCap) {
+        E* temp = new E[newCap];
+        for (int i = 0; i < size; i++) {
+            temp[i] = data[i];
+        }
 
-}
+        // 释放原数组内存
+        delete[] data;
+
+        data = temp;
+        cap = newCap;
+    }
+
+    ~MyArrayList() {
+        delete[] data;
+    }
+};
+
 int main() {
+    MyArrayList<int> arr(3);
+    for(int i=0; i<5; i++) {
+        arr.addLast(i);
+    } // 0 1 2 3 4
 
+    arr.remove(4); // 0 1 2 3
+    arr.add(1, 9); // 0 9 1 2 3
+    arr.addFirst(100); // 100 0 9 1 2 3
+
+    int val = arr.removeLast(); // 100 0 9 1 2
+
+    for(int i=0;i < arr.getSize(); i++) {
+        cout<< arr.get(i) <<endl;
+    }
     return 0;
 }
