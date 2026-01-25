8a44550f3aa23189920ad245db6c1678225e4848
YYYYXL1004 <147621608+YYYYXL1004@users.noreply.github.com>
2026-01-26 00:59:38 +0800

    48:顺时针90度旋转矩阵 54:顺时针访问二维数组 59:顺时针输入二维数组

---
+++ 48.旋转图像.cpp
@@ -0,0 +1,39 @@
+/*
+ * @lc app=leetcode.cn id=48 lang=cpp
+ * @lcpr version=30307
+ *
+ * [48] 旋转图像
+ */
+
+// @lc code=start
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        // 先沿主对角线镜像翻转
+        for(int i = 0; i < n; i++) {
+            for(int j = i; j < n; j++) {
+                swap(matrix[i][j], matrix[j][i]);
+            }
+        }
+        // 然后反转二维矩阵每一行
+        for(auto &row : matrix) {
+            reverse(row.begin(), row.end());
+        }
+    }
+};
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [[1,2,3],[4,5,6],[7,8,9]]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]]\n
+// @lcpr case=end
+
+ */
+
