1edaf32d432378df3d0b134f4e451fc1e111eed4
YYYYXL1004 <147621608+YYYYXL1004@users.noreply.github.com>
2026-02-06 21:09:11 +0800

    二叉树的4种遍历方式。前中后递归遍历，层次遍历使用队列辅助

---
+++ 94.二叉树的中序遍历.cpp
@@ -0,0 +1,56 @@
+/*
+ * @lc app=leetcode.cn id=94 lang=cpp
+ * @lcpr version=30307
+ *
+ * [94] 二叉树的中序遍历
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> res;
+    vector<int> inorderTraversal(TreeNode* root) {
+        traverse(root);
+        return res;
+    }
+    void traverse(TreeNode* root) {
+        if(root == nullptr) return ;
+        traverse(root->left);
+        res.push_back(root->val);
+        traverse(root->right);
+    }
+};
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [1,null,2,3]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1,2,3,4,5,null,8,null,null,6,7,9]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// []\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1]\n
+// @lcpr case=end
+
+ */
+
