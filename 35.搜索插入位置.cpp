3ec41f52caeb83483acebb6b0df3421ab4ef9d18
Y2498378289 <Y2498378289@noreply.gitcode.com>
2026-02-15 23:53:51 +0800

    2.15学习记录更新v2:LIS问题

--- 35.搜索插入位置.cpp
+++ 35.搜索插入位置.cpp
@@ -8,22 +8,24 @@
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
-        int l =0;
-        int r = nums.size()-1;
-        int mid = 0;
-        // if(target > nums[r]) return r+1;
-        // else if(target < nums[l]) return 0;
-        while(l<=r) {
-            mid = l + (r-l)/2;
-            if(target < nums[mid]) {
-                r = mid - 1;
-            } else if(target > nums[mid]) {
-                l = mid + 1;
-            } else {
-                return mid;
-            }
-        }
-        return l;
+        // int l = 0;
+        // int r = nums.size() - 1;
+        // // if(target > nums[r]) return r+1;
+        // // else if(target < nums[l]) return 0;
+        // while(l <= r) {
+        //     int mid = l + (r - l) / 2;
+        //     if(target < nums[mid]) {
+        //         r = mid - 1;
+        //     } else if(target > nums[mid]) {
+        //         l = mid + 1;
+        //     } else {
+        //         return mid;
+        //     }
+        // }
+        // return l;
+
+        int pos = lower_bound(nums.begin(), nums.end(), target) - nums.begin();
+        return pos;
     }
 };
 // @lc code=end
