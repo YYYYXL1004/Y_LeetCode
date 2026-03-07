46c84c82f88e35a8b03bd455119ca5ec8c369945
Y2498378289 <Y2498378289@noreply.gitcode.com>
2026-03-07 21:25:44 +0800

    重构目录，分为LeetCode,LanQiao,TianTi三大块

---
+++ LeetCode/404.左叶子之和.cpp
@@ -0,0 +1,40 @@
+/*
+ * @lc app=leetcode.cn id=404 lang=cpp
+ * @lcpr version=30400
+ *
+ * [404] 左叶子之和
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int sumOfLeftLeaves(TreeNode* root) {
+        
+    }
+};
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [3,9,20,null,null,15,7]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1]\n
+// @lcpr case=end
+
+ */
+
